96d38a58992271fc135a2da6dcd422ea751a8163
Divij <divij.shrivastava@gmail.com>
2024-09-23 02:55:43 +0530

    adds prod configs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+ firebaseConfig:{
+    apiKey: "AIzaSyDCXHV7muhUAD1nH33fD36FnLAIhl-RyrQ",
+    authDomain: "recipeapp-fcddc.firebaseapp.com",
+    projectId: "recipeapp-fcddc",
+    storageBucket: "recipeapp-fcddc.appspot.com",
+    messagingSenderId: "760240362415",
+    appId: "1:760240362415:web:341ed643a1aab3fac0555a",
+    measurementId: "G-STMCZSLGVZ"
+ }
+};
